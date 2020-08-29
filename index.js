17a0271c5ea3dbe6dded3b4945dccaa5d20f719d
Park Sang Jun <iteach12@gmail.com>
2020-08-29 22:15:37 +0900

    2nd

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let myTitle = document.getElementById("title");
+const title = document.querySelector("#title");
 
-console.log(myTitle);
+let result = 0;
 
-myTitle.title = "박상준";
+function countOfClick(){
+    result += 1;
+    console.log(`클릭 횟수는 ${result} 입니다.`);
+}
+
+
+title.addEventListener("click", countOfClick);
