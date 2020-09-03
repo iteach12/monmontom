1564398ab0ec221dc13273da471ea00fda77abce
Park Sang Jun <iteach12@gmail.com>
2020-09-03 22:18:40 +0900

    3#6 js

--- greeting.js
+++ greeting.js
@@ -1,12 +1,43 @@
 const form = document.querySelector(".js-form"),
-  input = form.querySelector("input");
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser";
+const USER_LS = "currentUser",
+ SHOWING_CN = "showing";
+
+
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+ function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+
+function paintGreeting(text){
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+
+}
 
 function loadName() {
-  const currentUserName = localStorage.getItem(USER_LS);
-  if (currentUserName === null) {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // She is not
+    askForName();
   } else {
+    // She is
+    paintGreeting(currentUser);
   }
 }
 function init() {
